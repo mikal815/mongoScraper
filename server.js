ddef74576c9d6586355bf920b8b24c2d066d8650
Mike M <mikal815@users.noreply.github.com>
2018-09-20 23:38:14 -0400

    heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -35,9 +35,9 @@ mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
-app.get("/", function(req, res){
-  res.render('home');
-})
+// app.get("/", function(req, res){
+//   res.render('home');
+// })
 
 // A GET route for scraping The Onion's website
 app.get("/scrape", function(req, res) {
